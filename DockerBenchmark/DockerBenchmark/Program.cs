e14e4cf077e9d46455d05fe7f131af4bdfb83be1
Yogev <miz.yogev@gmail.com>
2019-07-22 22:30:30 +0300

    Make docker hang untill stopped + exception handling

--- DockerBenchmark/DockerBenchmark/Program.cs
+++ DockerBenchmark/DockerBenchmark/Program.cs
@@ -1,10 +1,14 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using DockerBenchmark.OS;
 using System;
 using System.Linq;
+using System.Threading;
 
 namespace DockerBenchmark
 {
@@ -14,18 +18,32 @@ namespace DockerBenchmark
         {
             Console.WriteLine("About To Start");
 
-            var config = DefaultConfig.Instance.With(Job.Default.With(InProcessEmitToolchain.Instance));
+            try
+            {
+                var config = DefaultConfig.Instance
+                    .With(Job.Default.With(InProcessEmitToolchain.Instance));
+                    //.With(CsvMeasurementsExporter.Default, CsvExporter.Default, RPlotExporter.Default);
 
-            var benchmarkClasses = 
-                typeof(Program).Assembly.GetTypes()
-                    .Where(_ => _.IsClass && _.GetMethods().Any(m => m.GetCustomAttributes(typeof(BenchmarkAttribute), false).Length > 0))
-                    .ToList();
+                var benchmarkClasses =
+                    typeof(Program).Assembly.GetTypes()
+                        .Where(_ => _.IsClass && _.GetMethods().Any(m => m.GetCustomAttributes(typeof(BenchmarkAttribute), false).Length > 0))
+                        .ToList();
 
-
-            benchmarkClasses.ForEach(_ => BenchmarkRunner.Run(_, config));
-            //var summary = BenchmarkRunner.Run<MutexBenchmark>(config);
-
+                benchmarkClasses.ForEach(_ => BenchmarkRunner.Run(_, config));
+                //var summary = BenchmarkRunner.Run<MutexBenchmark>(config);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
             Console.WriteLine("Finished");
+
+            while (true)
+            {
+                Console.WriteLine("Hanging");
+                Thread.Sleep(10000);
+            }
         }
     }
 }
